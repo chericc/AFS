40812852fe847a77ea659f1d34d2ebe6df5dc30a
chericc <1461021702@qq.com>
2026-01-18 13:53:46 +0000

    feat(file_provider): fs iterator

--- src/tools/file_provider/file_provider.cpp
+++ src/tools/file_provider/file_provider.cpp
@@ -25,7 +25,7 @@ bool FolderFileProvider::setFolderPath(std::string const& folderPath)
 
 bool FolderFileProvider::reset()
 {
-    _ctx->folderPath.clear();
+    _ctx->folderIterator = nullptr;
     return true;
 }
 
@@ -35,12 +35,53 @@ bool FolderFileProvider::next()
     do {
         if (!fs::exists(_ctx->folderPath)) {
             XLOG_DEBUG("not exist: {}", _ctx->folderPath);
+            break;
         }
+
+        if (!fs::is_directory(_ctx->folderPath)) {
+            XLOG_DEBUG("not directory: {}", _ctx->folderPath);
+            break;
+        }
+
+        std::error_code err{};
+        fs::directory_options options{};
+        
+        if (!_ctx->folderIterator) {
+            _ctx->folderIterator = std::make_unique<fs::recursive_directory_iterator>(
+                _ctx->folderPath,
+                options, 
+                err
+            );
+            if (err) {
+                XLOG_DEBUG("folder iterate failed: {}", err.message());
+                break;
+            }
+
+            haveNextFlag = true;
+            break;
+        }
+
+        _ctx->folderIterator->increment(err);
+
+        if (err) {
+            XLOG_DEBUG("increment failed: {}", err.message());
+            break;
+        }
+
+        haveNextFlag = true;
     } while (false);
-    return true;
+
+    if (!haveNextFlag) {
+        _ctx->folderIterator = nullptr;
+    }
+
+    return haveNextFlag;
 }
 
 std::string FolderFileProvider::path()
 {
-    
+    if (_ctx->folderIterator) {
+        return (**(_ctx->folderIterator)).path();
+    }
+    return {};
 }
