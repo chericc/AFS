fed296d94cb93c87839ff7e814fcf1b611847651
chericc <1461021702@qq.com>
2026-02-13 07:52:51 +0000

    feat: db tmp upload

--- src/tools/db/db.cpp
+++ src/tools/db/db.cpp
@@ -1,4 +1,15 @@
 #include "db.hpp"
+#include "sqlpp11/sqlpp11.h"
+#include "sqlpp11/sqlite3/sqlite3.h"
+
+#include "db_connection.h"
+
+namespace {
+
+sqlpp::sqlite3::connection_pool s_db_pool{};
+thread_local db_connection s_dbc{s_db_pool};
+
+}
 
 struct Db::Context {
     
@@ -12,5 +23,13 @@ Db& Db::get()
 
 bool Db::init(Db::Param const& param)
 {
+    constexpr size_t db_pool_capacity = 5;
+
+    auto config = std::make_shared<sqlpp::sqlite3::connection_config>();
+    config->debug = false;
+    config->path_to_database = param.dbPath;
+    config->flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_NOMUTEX;
+    s_db_pool.initialize(config, db_pool_capacity);
     return true;
 }
+
---
+++ src/tools/db/demo/main.cpp
@@ -0,0 +1,11 @@
+#include "db.hpp"
+
+#include "sqlpp11/sqlpp11.h"
+#include "sqlpp11/sqlite3/sqlite3.h"
+
+int main()
+{
+    
+
+    return 0;
+}
